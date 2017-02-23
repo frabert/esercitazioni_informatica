52d1e727a5ef807912848897d307d733e0923b18
Francesco Bertolaccini <bertolaccinifrancesco@gmail.com>
2017-02-23 10:56:41 +0100

    Modifica per correttezza

--- algo1617/Lezione 1/Lez1Es5.c
+++ algo1617/Lezione 1/Lez1Es5.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int sommaDispari(int n) {
-    if(n == 0) return 1;
-    return (2*n + 1) + sommaDispari(n - 1);
+    if(n == 0) return 0;
+    return (2 * (n - 1) + 1) + sommaDispari(n - 1);
 }
 
 int main(void) {
     int n;
     scanf("%d", &n);
-    
-    printf("%d", sommaDispari(n - 1));
-    
+
+    printf("%d", sommaDispari(n));
+
     return 0;
 }
